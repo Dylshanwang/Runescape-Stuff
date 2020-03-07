289f49fe4388f7c04e469a76a95872dadf4d1d5a
Dylshanwang <dylan.dowling@gmail.com>
2020-03-07 20:08:49 +1100

    trying to use pandas to do everything

--- Scripts/GE.py
+++ Scripts/GE.py
@@ -17,14 +17,35 @@ url = "https://rsbuddy.com/exchange/summary.json"
 response = request.urlopen(url)
 data = json.loads(response.read())
 
-def fetch_item_price(item_id):
-    item_price = data[item_id]["overall_average"]
-    return item_price
+item_info_dict = {}
 
+
+def fetch_item_info(item_id):
+    item_price = data[item_id]["overall_average"]
+    item_name = data[item_id]["name"]
+    item_info_dict[item_name] = item_price
+    return item_info_dict
+
+
+# sang, claws, dwh, d pick
+item_id_list = ["22481", "13652", "13576", "11920"]
+for item in item_id_list:
+    fetch_item_info(item)
+
+print(item_info_dict)
 
 csv_file = "C:/Projects/Python/Runescape-Stuff/ItemPrices.csv"
+csv_columns = ["Item Name", datetime]
 
-# appending new price column to csv
-# csv_input = pd.read_csv(csv_file)
-# csv_input[datetime] = [item_list[0], item_list[1]]
-# csv_input.to_csv(csv_file, index=False)
+csv_input = pd.read_csv(csv_file, header=0)
+csv_input.columns = ["Item", datetime]
+csv_input["Item"] = item_info_dict.keys()
+csv_input[datetime] = item_info_dict.values()
+csv_input.to_csv(csv_file, index=False)
+
+# with open(csv_file, 'w', newline="") as f:
+#     writer = csv.writer(f)
+#     for key, value in item_info_dict.items():
+#         writer.writerow([key, value])
+
+
